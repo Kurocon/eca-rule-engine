cd33a4fa785aa52b2739baf65aca154bf1ee1ca9
kevalb <k.j.alberts@student.utwente.nl>
2013-10-30 10:26:26 +0100

    Changes of 29-10-2013

--- src/modules/custom_functions.py
+++ src/modules/custom_functions.py
@@ -1,7 +1,13 @@
 import csv
 def read_csv(filename):
 	final_list = []
-	csv_file = csv.reader(open(filename, newline=''), delimiter=',', quotechar='"')
-	for row in csv_file:
-		for element in row:
-			
+	#csv_file = csv.reader(, delimiter=',', quotechar='"')
+	# for row in csv_file:
+	# 	incremental_list = []
+	# 	for element in row:
+	# 		incremental_list.append(element)
+	# 	final_list.append(incremental_list)
+	# print(final_list)
+
+	csv = open(filename, newline='\n', encoding='utf-8').read()
+	print(csv)
